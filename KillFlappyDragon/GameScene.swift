a08db8460492142fb81cfc0510d7f499fb3f9713
jsik <survivor2199@yahoo.com>
2017-10-10 23:21:40 -0700

    added bg

---
+++ KillFlappyDragon/GameScene.swift
@@ -0,0 +1,91 @@
+//
+//  GameScene.swift
+//  KillFlappyDragon
+//
+//  Created by Jean Sik on 2017/10/10.
+//  Copyright © 2017 Jean Sik. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    
+    var plane:SKSpriteNode!
+
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
+    
+    override func didMove(to view: SKView) {
+        self.backgroundColor = .white
+        self.addBackground()
+    }
+
+    func addBackground() {
+        for index in 0..<2 {
+            let bg = SKSpriteNode(imageNamed: "Background")
+            bg.position = CGPoint(x: index * Int(bg.size.width), y: 0)
+            bg.anchorPoint = CGPoint(x: 0, y: 0)
+            bg.name = "background"
+            
+            self.addChild(bg)
+        }
+    }
+
+    func touchDown(atPoint pos : CGPoint) {
+//        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+//            n.position = pos
+//            n.strokeColor = SKColor.green
+//            self.addChild(n)
+//        }
+    }
+    
+    func touchMoved(toPoint pos : CGPoint) {
+//        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+//            n.position = pos
+//            n.strokeColor = SKColor.blue
+//            self.addChild(n)
+//        }
+    }
+    
+    func touchUp(atPoint pos : CGPoint) {
+//        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+//            n.position = pos
+//            n.strokeColor = SKColor.red
+//            self.addChild(n)
+//        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        if let label = self.label {
+//            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+//        }
+//        
+//        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        self.enumerateChildNodes(withName: "background", using: {(node, stop) -> Void in
+            if let bg = node as? SKSpriteNode {
+                bg.position = CGPoint(x: bg.position.x - 3.0, y: bg.position.y)
+                
+                if bg.position.x <= -bg.size.width {
+                    bg.position = CGPoint(x: bg.position.x + bg.size.width * 2, y: bg.position.y)
+                }
+            }
+        })
+    }
+}
