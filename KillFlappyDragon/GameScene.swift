3af7b337e57a1c9a85f6c6e855cecb73d3141eec
jsik <survivor2199@yahoo.com>
2017-10-11 00:59:51 -0700

    added bullet

--- KillFlappyDragon/GameScene.swift
+++ KillFlappyDragon/GameScene.swift
@@ -15,6 +15,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var plane:SKSpriteNode!
     var moveUpPlane: SKAction!
     var moveDownPlane: SKAction!
+    var deadPlane: SKAction!
+    
     var countFlappyDragon = 0
     var lastFlappyDragon: TimeInterval = 0
     
@@ -42,6 +44,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         moveUpPlane = SKAction.moveBy(x: 0, y: 30, duration: 0.2)
         moveDownPlane = SKAction.moveBy(x: 0, y: -30, duration: 0.2)
+        deadPlane = SKAction.moveBy(x: 0, y: -1000, duration: 1)
             
         self.addChild(plane)
     }
@@ -52,6 +55,18 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         flappydragon.setScale(0.05)
         flappydragon.name = "flappydragon"
         self.addChild(flappydragon)
+    }
+    
+    func addProjectile() {
+        let projectile = SKSpriteNode(imageNamed: "Bullet")
+        projectile.position = CGPoint(x: self.plane.position.x, y: self.plane.position.y)
+        projectile.setScale(0.25)
+        projectile.name = "projectile"
+        self.addChild(projectile)
+    }
+    
+    func fireProjectile() {
+        self.addProjectile()
     }
     
     func touchDown(atPoint pos : CGPoint) {
@@ -78,9 +93,32 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
 //        }
     }
     
+//    func didBegin(_ contact: SKPhysicsContact) {
+//        var firstBody:SKPhysicsBody
+//        var secondBody = SKPhysicsBody()
+//        
+//        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask
+//        {
+//            firstBody = contact.bodyA
+//            secondBody = contact.bodyB
+//        }
+//        else
+//        {
+//            firstBody = contact.bodyB
+//            secondBody = contact.bodyA
+//        }
+//        
+//        guard firstBody.node != nil else { return }
+//        (firstBody.node as! SKSpriteNode).removeFromParent()
+//    }
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for _: AnyObject in touches {
-            if plane.position.y < 300 {
+        for touch: AnyObject in touches {
+            let location = touch.location(in: self)
+            if location.x > plane.position.x {
+                self.fireProjectile()
+            }
+            if location.y > plane.position.y {
                 plane.run(moveUpPlane)
             } else if plane.position.y > 50 {
                 plane.run(moveDownPlane)
@@ -88,7 +126,19 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         }
 
     }
-
+    
+    func moveProjectile() {
+        self.enumerateChildNodes(withName: "projectile", using: {(node, stop) -> Void in
+            if let projectile = node as? SKSpriteNode {
+                projectile.position = CGPoint(x: projectile.position.x + 8.0, y: projectile.position.y)
+                
+                if projectile.position.x > 10000 {
+                    projectile.removeFromParent()
+                }
+            }
+        })
+    }
+    
     func moveFlappyDragon() {
         self.enumerateChildNodes(withName: "flappydragon", using: {(node, stop) -> Void in
             if let flappydragon = node as? SKSpriteNode {
@@ -104,6 +154,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
 
     override func update(_ currentTime: TimeInterval) {
         self.moveFlappyDragon()
+        self.moveProjectile()
         if (self.countFlappyDragon < self.MAX_DRAGON && self.lastFlappyDragon < currentTime - 1 ) {
             self.countFlappyDragon += 1
             self.lastFlappyDragon = currentTime + 1
