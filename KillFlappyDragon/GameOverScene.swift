02178a973bbb9db273791b5346f58e52dd3add8d
jsik <survivor2199@yahoo.com>
2017-10-15 12:19:11 -0700

    added counter

--- KillFlappyDragon/GameOverScene.swift
+++ KillFlappyDragon/GameOverScene.swift
@@ -9,38 +9,40 @@
 import SpriteKit
 
 class GameOverScene: SKScene {
+    var textTop: CGFloat = 0
     var winTheGame: Bool = false
     var flappyDragon: String = "0"
     
     override init(size: CGSize) {
         super.init(size: size)
-        
+        textTop = self.size.height/2 + 80
         self.backgroundColor = .white
-        let textTop = self.size.height/2 + 80
         let gameOverLabel = SKLabelNode(fontNamed: "Arial")
         gameOverLabel.text = "Game Over"
         gameOverLabel.fontSize = 40
         gameOverLabel.fontColor = .black
         gameOverLabel.position = CGPoint(x: self.size.width/2,y: textTop)
         self.addChild(gameOverLabel)
-        
+    }
+    
+    override func didMove(to view: SKView) {
         let resultLabel = SKLabelNode(fontNamed: "Arial")
-        resultLabel.text = (winTheGame ? "You won the game! " : "Sorry! You lost! ")
+        resultLabel.text = (winTheGame ? "Congratulations! You won!" : "Oh no! You lost :(")
         resultLabel.fontSize = 30
-        resultLabel.fontColor = .black
-        resultLabel.position = CGPoint(x: self.size.width/2,y: textTop - 35)
+        resultLabel.fontColor = .red
+        resultLabel.position = CGPoint(x: self.size.width/2,y: textTop - 40)
         self.addChild(resultLabel)
         
         if (!winTheGame) {
-            print(self.flappyDragon)
-//            let countLabel = SKLabelNode(fontNamed: "Arial")
-//            resultLabel.text =
-//            resultLabel.fontSize = 30
-//            resultLabel.fontColor = .black
-//            resultLabel.position = CGPoint(x: self.size.width/2,y: self.textTop - 35)
-//            self.addChild(resultLabel)
+            let countLabel = SKLabelNode(fontNamed: "Arial")
+            countLabel.text = "You killed " + self.flappyDragon + " Flappy Dragons"
+            countLabel.fontSize = 30
+            countLabel.fontColor = .black
+            countLabel.position = CGPoint(x: self.size.width/2,y: self.textTop - 100)
+            self.addChild(countLabel)
         }
     }
+    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
